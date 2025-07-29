7c809a41d1a655f33a40467e56359abc84b99299
theadityamaurya <adityamaurya712@gmail.com>
2025-07-29 20:08:44 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float r,c,a;
+    printf("enter radius=");
+    scanf("%f",&r);
+    float pi=3.14;
+    c=2*pi*r;
+    a=pi*r*r;
+    printf("\ncircumference of a circle=%f",c);
+    printf("\narea of a circle=%f",a);
+
+}
